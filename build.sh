41f77e52296cc0103f3f50f934d785e1a039c056
AadyaPandey <aadyapandey2004@gmail.com>
2025-08-07 17:43:09 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Installing Python and required packages..."
-
-# Update package lists and install Python 3 & pip
-apt-get update && apt-get install -y python3 python3-pip
-
-# Install Python packages from the correct path
-pip3 install -r backend/python/requirements.txt
